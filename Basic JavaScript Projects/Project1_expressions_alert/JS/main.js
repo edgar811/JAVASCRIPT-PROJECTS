1485e98251ba705a13f940726799a6b1c683b95b
edgar811 <edgargonzalez1989@gmail.com>
2025-01-30 11:39:04 -0500

    add

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+ window.alert("This is an alert message!");
+
+var myFamily = "The Gonzalez Family", Dad = "Edgar", Mom = "Kyla", Son1 = "Lucas", Son2 = "Oscar", Daughter = "Sophia" //Here is a variable wth multiple values
+
+document.write(Mom, "<br>");
+
+document.write(3 + 3, "<br>");
+
+var sent1 = "This is the beginnning of the string "
+var sent2 = "and this is the end of the string!"
+
+document.write(sent1 + sent2)
