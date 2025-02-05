60c8bf7dac53a93743b0d6af9fea0004de4e8f98
edgar811 <edgargonzalez1989@gmail.com>
2025-02-05 10:07:50 -0500

    adding these changes

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -28,3 +28,26 @@ function searchWord() {
     document.getElementById("searchOutput").innerHTML = spot;
 }
 
+//This function will return a integer as a string
+function number2word() {
+    var x = 3289;
+    document.getElementById("Word").innerHTML = x.toString();
+}
+
+//This function will make a long number precision
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+
+//This function will use the toFixed method to make a number a string rounded to a specific decimal
+function fix_Number() {
+    var x = 8.93652;
+    document.getElementById("Fixed").innerHTML = x.toFixed(2);
+}
+
+//This function will use the valueOf method
+function value_Method() {
+    var text = "Hello World!";
+    document.getElementById("Value").innerHTML = text.valueOf();
+}
