1b0a9e77a3748ff8e3c13f6c06779d85da01a08c
edgar811 <edgargonzalez1989@gmail.com>
2025-02-04 22:54:23 -0500

    adding

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,30 @@
+//This function combines all of the variables and makes a whole sentence with them
+function connecting_Strings() {
+    var number1 = "I am making "
+    var number2 = "this into "
+    var number3 = "one whole sentence!"
+    var allNumbers = number1.concat(number2, number3);
+    document.getElementById("Concatenate").innerHTML = allNumbers;
+}
+
+//This function will use the strong slice method to display a part of the string
+function slice_Method() {
+    var words = "Here we are going to just display a section of this string!";
+    var cut = words.slice(10, 22);
+    document.getElementById("Slice").innerHTML = cut;
+}
+
+//This function will be used to convert text to uppercase
+function convertToUpperCase() {
+    var text = "The pet shop is open!";
+    var upperText = text.toUpperCase();
+    document.getElementById("upperCaseOutput").innerHTML = upperText;
+}
+
+//This function will search for a word in the string 
+function searchWord() {
+    var sentence = "Here will be searching for one of the words in this sentence!";
+    var spot = sentence.search("one")
+    document.getElementById("searchOutput").innerHTML = spot;
+}
+
