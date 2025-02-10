826e01b64451cc029bda5cf6dad73cf8d7e2348b
edgar811 <edgargonzalez1989@gmail.com>
2025-02-10 15:04:40 -0500

    add

--- Advanced_JavaScript_Projects/JavaScript/JavaScript_1.js
+++ Advanced_JavaScript_Projects/JavaScript/JavaScript_1.js
@@ -24,3 +24,40 @@ function pet_Function() {
     }
     document.getElementById("Output").innerHTML = petChoice;
 }
+
+function changeText() {
+    let elements = document.getElementsByClassName("example");
+    for (let item of elements) {
+        item.textContent = "This is the new sentence!";
+    }
+}
+
+//This function is for the CANVAS CHALLENGE
+function drawGradient() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    var grd = ctx.createLinearGradient(0, 0, 200, 0);
+    grd.addColorStop(0, "red");
+    grd.addColorStop(1, "white");
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(10, 10, 150, 80);
+}
+
+drawGradient();
+
+//This function is for the GRADIENT COLOR CHALLENGE
+function drawGradientRectangle() {
+    const canvas = document.getElementById("myCanvas2");
+    const ctx = canvas.getContext("2d");
+
+    const grd = ctx.createLinearGradient(0, 0, 170, 0);
+    grd.addColorStop(0, "black");
+    grd.addColorStop(1, "white");
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(20, 20, 150, 100);
+}
+
+drawGradientRectangle();
